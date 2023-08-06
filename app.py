769e2d308d98059d224a933d717ce37571fe26d9
Prem Bamrung <prem.bamrung@torus-actions.fr>
2023-08-06 20:09:01 +0200

    Update app and requirements with pytube and streamlit chat

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import re
 from io import BytesIO
 
+import pytube
 import requests
 import streamlit as st
 from PIL import Image
+from streamlit_chat import message
 from youtube_transcript_api import YouTubeTranscriptApi
+
+st.cache_data()
 
 
 def extract_video_id(url):
@@ -15,6 +19,9 @@ def extract_video_id(url):
     if match:
         video_id = match[0]
     return video_id
+
+
+st.cache_data()
 
 
 def extract_subtitles(video_id, language):
@@ -30,6 +37,9 @@ def extract_text(subs):
     return text
 
 
+st.cache_data()
+
+
 def extract_thumbnail(vid_id):
     thumbnail_url = f"https://img.youtube.com/vi/{vid_id}/maxresdefault.jpg"
     try:
@@ -40,8 +50,31 @@ def extract_thumbnail(vid_id):
         st.write("Error displaying thumbnail.")
 
 
+st.cache_data()
+
+
+def extract_video_info(video_url):
+    try:
+        yt = pytube.YouTube(video_url)
+        video_info = {
+            "title": yt.title,
+            "channel": yt.author,
+            "date": yt.publish_date.date(),
+            "views": yt.views,
+            # 'comments': yt.embed_html
+        }
+        # # Get number of subscribers
+        # channel_url = f"https://www.youtube.com/channel/{yt.channel_id}"
+        # channel = pytube.Channel(channel_url)
+        # video_info['subscribers'] = channel.subscriber_count
+        return video_info
+    except Exception as e:
+        print(e)
+        return None
+
+
 def main():
-    st.title("Subtitle Extractor")
+    st.title("GPTube")
     st.write("Enter a YouTube video URL and select the language to extract subtitles:")
 
     url = st.text_input("YouTube URL:")
@@ -52,15 +85,27 @@ def main():
     if st.button("Extract"):
         if url:
             video_id = extract_video_id(url)
+            video_info = extract_video_info(url)
             if video_id:
                 subtitles = extract_subtitles(video_id, language.lower())
                 if subtitles:
                     text = extract_text(subtitles)
                     if text:
                         st.success("Subtitles extracted successfully!")
-                        img_thumbnail = extract_thumbnail(video_id)
-                        st.image(img_thumbnail)
-                        st.write(text)
+                        col1, col2 = st.columns(2)
+                        with col1:
+                            img_thumbnail = extract_thumbnail(video_id)
+                            st.subheader(video_info["title"])
+                            st.image(img_thumbnail)
+                            st.text(
+                                video_info["channel"] + " " + str(video_info["date"])
+                            )
+                            st.text(f"Words count : {len(text.split())}")
+                            with st.expander("See full text"):
+                                st.write(text)
+                        with col2:
+                            message("My message")
+                            message("Hello bot!", is_user=True)
                 else:
                     st.warning(
                         f"No {language} subtitles found for the given video URL."
